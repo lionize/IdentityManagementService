6ea5fe2a59290af1ca849881d4d3bb8c5e6c66cc
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-10 13:13:27 +0300

    Create AccountsController.cs

---
+++ src/Controllers/AccountsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TIKSN.Lionize.IdentityManagementService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController
+    {
+        [HttpPost("SignUp")]
+        public async Task<SignUpResponse> SignUp([FromBody]SignUpRequest request)
+        {
+            var validator = new SignUpRequestValidator();
+
+            var validation = validator.Validate(request);
+
+            if (validation.IsValid)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                return new SignUpResponse { IsError = true, ErrorMessage = validation.Errors.First().ErrorMessage };
+            }
+        }
+    }
+}
