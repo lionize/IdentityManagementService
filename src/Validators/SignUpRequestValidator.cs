519a3488794b55f6f389a784497a93c1915f7195
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-10 13:22:07 +0300

    Create SignUpRequestValidator.cs

---
+++ src/Validators/SignUpRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Lionize.IdentityManagementService.ApiModels;
+
+namespace TIKSN.Lionize.IdentityManagementService.Validators
+{
+    public class SignUpRequestValidator : AbstractValidator<SignUpRequest>
+    {
+        public SignUpRequestValidator()
+        {
+            RuleFor(x => x.Username).NotNull().NotEmpty();
+            RuleFor(x => x.Password).NotNull().NotEmpty();
+            RuleFor(x => x.PasswordConfirmation).NotNull().NotEmpty().Equal(x => x.Password).WithMessage("Password confirmation is incorrect.");
+        }
+    }
+}
