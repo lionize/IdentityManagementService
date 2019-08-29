7a19ecbfc958b0109830ecbe2959290c13da7485
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-29 22:29:25 +0300

    Create AddApiSecretOptions.cs

---
+++ src/Shell/AddApiSecretOptions.cs
@@ -0,0 +1,9 @@
+﻿using CommandLine;
+
+namespace TIKSN.Lionize.IdentityManagementService.Shell
+{
+    [Verb("AddApiSecret", HelpText = "Add Api Secret.")]
+    public class AddApiSecretOptions
+    {
+    }
+}
