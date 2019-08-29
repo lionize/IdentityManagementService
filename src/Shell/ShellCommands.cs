915b509c2e4b905913a91eb9e26930f351d62903
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-29 22:46:10 +0300

    Create ShellCommands.cs

---
+++ src/Shell/ShellCommands.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace TIKSN.Lionize.IdentityManagementService.Shell
+{
+    public class ShellCommands : IShellCommands
+    {
+        private readonly byte[] _buffer;
+        private readonly Random _random;
+
+        public ShellCommands(Random random)
+        {
+            _random = random ?? throw new ArgumentNullException(nameof(random));
+            _buffer = new byte[64];
+        }
+
+        public Task AddApiSecretAsync(AddApiSecretOptions options)
+        {
+            var originalSecret = CreateOriginalSecret();
+            throw new NotImplementedException();
+        }
+
+        public Task AddClientSecret(AddClientSecretOptions options)
+        {
+            var originalSecret = CreateOriginalSecret();
+            throw new NotImplementedException();
+        }
+
+        private OriginalSecret CreateOriginalSecret()
+        {
+            _random.NextBytes(_buffer);
+
+            var originalString = Convert.ToBase64String(_buffer);
+
+            var originalStringSha256 = originalString.Sha256();
+
+            return new OriginalSecret(originalString, originalStringSha256);
+        }
+    }
+}
