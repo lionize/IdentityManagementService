a295275c25af7cbba53b2547365b71c59ed04bce
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-29 22:45:52 +0300

    Create OriginalSecret.cs

---
+++ src/Shell/OriginalSecret.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TIKSN.Lionize.IdentityManagementService.Shell
+{
+    public class OriginalSecret
+    {
+        public OriginalSecret(string originalSecret, string hashedSecret)
+        {
+            Original = originalSecret ?? throw new System.ArgumentNullException(nameof(originalSecret));
+            Hashed = hashedSecret ?? throw new System.ArgumentNullException(nameof(hashedSecret));
+        }
+
+        public string Hashed { get; }
+
+        public string Original { get; }
+    }
+}
