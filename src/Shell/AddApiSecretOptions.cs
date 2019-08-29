c6fb7831f887bec00cb4729868d9ff881b212ff9
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-29 23:04:16 +0300

    Update AddApiSecretOptions.cs

--- src/Shell/AddApiSecretOptions.cs
+++ src/Shell/AddApiSecretOptions.cs
@@ -5,5 +5,12 @@ namespace TIKSN.Lionize.IdentityManagementService.Shell
     [Verb("AddApiSecret", HelpText = "Add Api Secret.")]
     public class AddApiSecretOptions
     {
+        public AddApiSecretOptions(int id)
+        {
+            Id = id;
+        }
+
+        [Option]
+        public int Id { get; }
     }
 }
