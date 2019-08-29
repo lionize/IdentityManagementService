ef6f7dc24f4a31cab5403875d52a28028892edfc
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-29 22:39:41 +0300

    Create IShellCommands.cs

---
+++ src/Shell/IShellCommands.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace TIKSN.Lionize.IdentityManagementService.Shell
+{
+    public interface IShellCommands
+    {
+        Task AddApiSecretAsync(AddApiSecretOptions options);
+
+        Task AddClientSecret(AddClientSecretOptions options);
+    }
+}
