48664e8fe05ffd6cdb9893dc90d8641c3256ca2c
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-29 22:29:31 +0300

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved. Licensed under the Apache
+// License, Version 2.0. See LICENSE in the project root for license information.
 
+using CommandLine;
 using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -9,10 +10,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
-using System;
+using System.Linq;
 using System.Threading.Tasks;
 using TIKSN.Lionize.IdentityManagementService.Data;
-using TIKSN.Shell;
+using TIKSN.Lionize.IdentityManagementService.Shell;
 
 namespace TIKSN.Lionize.IdentityManagementService
 {
@@ -53,20 +54,16 @@ namespace TIKSN.Lionize.IdentityManagementService
                 await scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.MigrateAsync();
             }
 
-            if (args.Length == 0)
-            {
-                host.Run();
-            }
-            else if(args.Length == 1 && "shell".Equals(args[0], StringComparison.OrdinalIgnoreCase))
-            {
-                var engine = host.Services.GetRequiredService<IShellCommandEngine>();
-
-                var thisAssembly = typeof(Program).Assembly;
-
-                engine.AddAssembly(thisAssembly);
-
-                await engine.RunAsync();
-            }
+            await Parser.Default.ParseArguments<AddApiSecretOptions, AddClientSecretOptions>(args)
+                .MapResult(
+                (AddApiSecretOptions options) => Task.CompletedTask,
+                (AddClientSecretOptions options) => Task.CompletedTask,
+                errors =>
+                {
+                    if (!errors.Any(x=> x.StopsProcessing))
+                        return host.RunAsync();
+                    return Task.CompletedTask;
+                });
         }
     }
 }
