338666dc845f2fac2715ed92004739ce05f85e7f
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-30 00:12:15 +0300

    Update AddClientSecretOptions.cs

--- src/Shell/AddClientSecretOptions.cs
+++ src/Shell/AddClientSecretOptions.cs
@@ -5,5 +5,12 @@ namespace TIKSN.Lionize.IdentityManagementService.Shell
     [Verb("AddClientSecret", HelpText = "Add Client Secret.")]
     public class AddClientSecretOptions
     {
+        public AddClientSecretOptions(int id)
+        {
+            Id = id;
+        }
+
+        [Option]
+        public int Id { get; }
     }
 }
